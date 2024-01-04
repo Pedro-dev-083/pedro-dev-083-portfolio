836edcb7371570591c2722b8dc38c01b70b7b09d
Pedro-dev-083 <pedroazevedo83@hotmail.com>
2024-01-03 22:24:01 -0300

    Solved the problem of passing the same info to RouteLink

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -6,19 +6,30 @@ import RouteLink from "./RouteLink/RouteLink";
 export default function Header() {
    const currentPath = usePathname();
 
-   //TODO: Try to use as a provider for the current RouteLinks
    function isCurrentPath(path: string) {
-      return path === currentPath ? "" : path;
+      return path === currentPath;
    }
 
    return (
       <HeaderMain>
          <div>
-            <RouteLink href={"/"}>Home</RouteLink>
+            <RouteLink iAmTheCurrentLink={isCurrentPath("/")} href={"/"}>
+               Home
+            </RouteLink>
             <br />
-            <RouteLink href={"/about"}>About</RouteLink>
+            <RouteLink
+               iAmTheCurrentLink={isCurrentPath("/about")}
+               href={"/about"}
+            >
+               About
+            </RouteLink>
             <br />
-            <RouteLink href={"/Contact"}>Contact</RouteLink>
+            <RouteLink
+               iAmTheCurrentLink={isCurrentPath("/contact")}
+               href={"/contact"}
+            >
+               Contact
+            </RouteLink>
          </div>
       </HeaderMain>
    );
--- app/components/Header/RouteLink/RouteLink.tsx
+++ app/components/Header/RouteLink/RouteLink.tsx
@@ -3,11 +3,17 @@ import Link from "next/link";
 interface LinkProps {
    children: React.ReactNode;
    href: string;
+   iAmTheCurrentLink: boolean;
 }
-export default function RouteLink({ children, href, ...props }: LinkProps) {
+export default function RouteLink({
+   children,
+   href,
+   iAmTheCurrentLink,
+   ...props
+}: LinkProps) {
    return (
       <Link href={href} {...props}>
-         {children}
+         {iAmTheCurrentLink ? `It is me: ${children}` : children}
       </Link>
    );
 }
