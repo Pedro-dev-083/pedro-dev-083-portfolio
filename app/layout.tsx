59e1c843fe22edb51d757db96cdc51318837801d
Pedro-dev-083 <pedroazevedo83@hotmail.com>
2024-01-04 23:01:41 -0300

    Starting the Not-found component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,30 @@
+"use client";
+import { usePathname } from "next/navigation";
 import Header from "./components/Header/Header";
 import GlobalStyles from "./theme/GlobalStyles";
+import { useEffect, useState } from "react";
+import NotFoundProvider from "./NotFoundProvider";
 
 export default function RootLayout({
    children,
 }: {
    children: React.ReactNode;
 }) {
+   const pathName = usePathname();
+   const [is404Page, setIs404Page] = useState(false);
+
+   useEffect(() => {
+      setIs404Page(pathName === "/404");
+   }, [pathName]);
+
    return (
       <html lang="pt-br">
          <body>
-            <GlobalStyles />
-            <Header />
-            {children}
+            <NotFoundProvider>
+               <GlobalStyles />
+               {!is404Page && <Header />}
+               {children}
+            </NotFoundProvider>
          </body>
       </html>
    );
