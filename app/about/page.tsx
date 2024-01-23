fe5a26d53053898bc22a192ec9c7dd4caec332d1
Pedro-dev-083 <pedroazevedo83@hotmail.com>
2024-01-22 22:15:35 -0300

    Starting the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,28 @@
 export default function About() {
    return (
       <div>
-         <p>This is the about screen</p>
+         <div>
+            {/*TODO: I think here will make more sense use my photo than the index page, and there instead my photo, maybe I can make something related to the world*/}
+            <p>Dev Photo</p>
+         </div>
+         <div>
+            <p>Little resume about me</p>
+            <p>Saying about my background as a dev</p>
+            <p>Where I live now and where I wish to go</p>
+         </div>
+         <div>
+            <p>Tell other stuff about me without code</p>
+            <p>Tell about games that I like</p>
+            <p>Youtube, cannot forget</p>
+            <p>Edit videos</p>
+         </div>
+         <div>
+            {/* TODO: In a technology that I click, I can show I project that I used that tecnology */}
+            <p>My stacks, which I am good, and which I am currently learning</p>
+         </div>
+         <div>
+            <p>Certifications and my resumé</p>
+         </div>
       </div>
    );
 }
