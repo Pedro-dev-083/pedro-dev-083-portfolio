d2edc3ae5ddd9df6d66047ef38881dcf08caae02
Pedro-dev-083 <pedroazevedo83@hotmail.com>
2024-01-23 21:51:34 -0300

    Adding some text and adding a link to the resume (in Portuguese)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,45 @@
+"use client";
+import Image from "next/image";
+import { PhotoContainer } from "./styled";
+
 export default function About() {
    return (
       <div>
          <div>
             {/*TODO: I think here will make more sense use my photo than the index page, and there instead my photo, maybe I can make something related to the world*/}
-            <p>Dev Photo</p>
+            <PhotoContainer>
+               <Image
+                  src={"https://avatars.githubusercontent.com/u/60190590?v=4"}
+                  alt={"Image of the developer"}
+                  width={250}
+                  height={250}
+                  style={{ borderRadius: "50%" }}
+               />
+            </PhotoContainer>
          </div>
          <div>
-            <p>Little resume about me</p>
+            <p>
+               I am an information systems developer with experience
+               professional at TopDown Sistemas, where I was responsible for
+               legacy code migrations and new feature development for healthcare
+               management systems, using ASP.NET MVC, jQuery and PL/SQL.
+               Additionally, I created training videos for share knowledge with
+               co-workers and gained experience with Azure DevOps, including
+               Pull Requests, control of versioning, deployments, sprints and
+               management of tasks. Before that, I worked as an intern at the
+               same company, where I conducted the migration of legacy code
+               screens, carried out automated tests and worked closely with the
+               methodology Scrum, using Azure DevOps and Git for project
+               control. I also have experience as an intern at COREN-RJ, where I
+               developed a web application full-stack and created tools for data
+               compilation, and how Young Apprentice in Administrative
+               Operations at Supergasbras - AMZ, where I worked with tax
+               documents and acquired skills essential for the corporate
+               environment. My focus is on continuous learning and development
+               of skills in various areas of technology. I am committed to
+               contributing to challenging projects and constantly evolving as
+               information systems developer.
+            </p>
             <p>Saying about my background as a dev</p>
             <p>Where I live now and where I wish to go</p>
          </div>
@@ -17,11 +50,21 @@ export default function About() {
             <p>Edit videos</p>
          </div>
          <div>
-            {/* TODO: In a technology that I click, I can show I project that I used that tecnology */}
+            {/* TODO: In a technology that I click, I can show I project that I used that technology */}
             <p>My stacks, which I am good, and which I am currently learning</p>
          </div>
          <div>
-            <p>Certifications and my resumé</p>
+            <p>Certifications</p>
+
+            <a
+               href={
+                  "https://portifolio-pedro-dev-083-bucket.s3.sa-east-1.amazonaws.com/Pedro+Souza+de+Azevedo.pdf"
+               }
+               target="_blank"
+               rel="noopener noreferrer"
+            >
+               Resume
+            </a>
          </div>
       </div>
    );
---
+++ app/about/styled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const PhotoContainer = styled.div`
+   padding: 5px;
+   margin: 5px;
+   position: relative;
+   overflow: "hidden";
+   height: 250px;
+   width: 250px;
+   border-radius: 50%;
+   border: "5px solid #F00";
+`;
