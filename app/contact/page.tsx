eac3233304ef2dc1c6f0b07dc0f6a33e83070eb0
Pedro-dev-083 <pedroazevedo83@hotmail.com>
2024-02-19 22:39:58 -0300

    Adding icons

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,24 @@
 "use client";
+import { GrLinkedin } from "react-icons/gr";
+import { MdEmail } from "react-icons/md";
 import { MainContainer } from "../styled";
 
 export default function Contact() {
+   const iconSize = 30;
+   const iconColor = "black";
    return (
       <MainContainer>
-         <a href="mailto:pedroazevedo83@hotmail.com">Email</a>
-         <p>Linkedin</p>
-         <p>Whatsapp</p>
+         <a href="mailto:pedroazevedo83@hotmail.com">
+            <MdEmail color={iconColor} size={iconSize} />
+         </a>
+         <a
+            href="https://www.linkedin.com/in/pedro-souza-de-azevedo/"
+            target="_blank"
+            rel="noopener noreferrer"
+         >
+            <GrLinkedin color={iconColor} size={iconSize} />
+         </a>
+         {/* <p>Whatsapp</p>  */}
       </MainContainer>
    );
 }
