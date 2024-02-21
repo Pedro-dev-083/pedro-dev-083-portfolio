0823db6b8ae150b2b078365114f924a88bf5d59e
Pedro-dev-083 <pedroazevedo83@hotmail.com>
2024-02-20 21:12:16 -0300

    Styling the icons

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,24 +1,28 @@
 "use client";
 import { GrLinkedin } from "react-icons/gr";
 import { MdEmail } from "react-icons/md";
-import { MainContainer } from "../styled";
+import { MainContainer } from "./styled";
 
 export default function Contact() {
    const iconSize = 30;
-   const iconColor = "black";
    return (
       <MainContainer>
-         <a href="mailto:pedroazevedo83@hotmail.com">
-            <MdEmail color={iconColor} size={iconSize} />
-         </a>
-         <a
-            href="https://www.linkedin.com/in/pedro-souza-de-azevedo/"
-            target="_blank"
-            rel="noopener noreferrer"
-         >
-            <GrLinkedin color={iconColor} size={iconSize} />
-         </a>
-         {/* <p>Whatsapp</p>  */}
+         <div>
+            E-mail:
+            <a href="mailto:pedroazevedo83@hotmail.com">
+               <MdEmail size={iconSize} />
+            </a>
+         </div>
+         <div>
+            Linkedin:
+            <a
+               href="https://www.linkedin.com/in/pedro-souza-de-azevedo/"
+               target="_blank"
+               rel="noopener noreferrer"
+            >
+               <GrLinkedin size={iconSize} />
+            </a>
+         </div>
       </MainContainer>
    );
 }
--- app/contact/styled.tsx
+++ app/contact/styled.tsx
@@ -2,6 +2,21 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div`
    display: flex;
-   justify-content: center;
+   flex-direction: column;
+   justify-content: space-between;
    align-items: center;
+   padding: 15%;
+   div {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 15%;
+      svg {
+         color: black;
+         transition: 0.3s ease-in-out;
+         &:hover {
+            color: blue;
+         }
+      }
+   }
 `;
