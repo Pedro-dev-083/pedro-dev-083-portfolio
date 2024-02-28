4c1bcaa3dc73670cd155fceee958e1337daf571b
Pedro-dev-083 <pedroazevedo83@hotmail.com>
2024-02-27 21:24:35 -0300

    Separating the project page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,6 +5,12 @@ export default function Projects() {
    return (
       <Container>
          <p>Projects Page</p>
+         <div>
+            <h1>Stacks</h1>
+         </div>
+         <div>
+            <h1>Projects</h1>
+         </div>
       </Container>
    );
 }
