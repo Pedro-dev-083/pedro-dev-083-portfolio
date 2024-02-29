bd25963b01497f7f658e83a28906b88576473e2e
Pedro-dev-083 <pedroazevedo83@hotmail.com>
2024-02-28 22:14:40 -0300

    Creating a StackContainer

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { Container } from "./styled";
+import { SiPython } from "react-icons/si";
+import { Container, StackContainer } from "./styled";
 
 export default function Projects() {
    return (
@@ -7,6 +8,9 @@ export default function Projects() {
          <p>Projects Page</p>
          <div>
             <h1>Stacks</h1>
+            <StackContainer>
+               <SiPython size={25} />
+            </StackContainer>
          </div>
          <div>
             <h1>Projects</h1>
