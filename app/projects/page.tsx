c3d62e6058936b0f6a4e7ae3867a59e8bc4e8818
Pedro-dev-083 <pedroazevedo83@hotmail.com>
2024-03-02 15:19:28 -0300

    Pagination Container

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,13 +1,29 @@
 "use client";
 import { SiPython } from "react-icons/si";
 import {
+   PaginationContainer,
    Container,
    NumberBox,
    ProjectContainer,
    StackContainer,
+   NumberContainer,
 } from "./styled";
+import { useState } from "react";
 
 export default function Projects() {
+   const [page, setPage] = useState(1);
+   const numbers = Array.from({ length: 6 }, (_, index) => index + 1);
+
+   const showLeftArrow = page > 1;
+   const showRightArrow = page < numbers.length - 1;
+
+   const handleClickLeft = () => {
+      setPage((prevPage) => (prevPage - 1 < 0 ? 0 : prevPage - 1));
+   };
+
+   const handleClickRight = () => {
+      setPage((prevPage) => prevPage + 1);
+   };
    return (
       <Container>
          <p>Projects Page</p>
@@ -22,9 +38,32 @@ export default function Projects() {
             <ProjectContainer>
                <p>Project</p>
             </ProjectContainer>
-            <NumberBox>
-               <p>1</p>
-            </NumberBox>
+            <PaginationContainer>
+               <NumberContainer>
+                  <div>
+                     {showLeftArrow && (
+                        <button onClick={handleClickLeft}>{"<"}</button>
+                     )}
+                  </div>
+                  {page <= 1 || page == numbers.length ? (
+                     <>
+                        <NumberBox>{numbers[0]}</NumberBox>
+                        <NumberBox>{numbers[1]}</NumberBox>
+                     </>
+                  ) : (
+                     numbers
+                        .slice(page - 1, page + 2)
+                        .map((number) => (
+                           <NumberBox key={number}>{number}</NumberBox>
+                        ))
+                  )}
+               </NumberContainer>
+               <div>
+                  {showRightArrow && (
+                     <button onClick={handleClickRight}>{">"}</button>
+                  )}
+               </div>
+            </PaginationContainer>
          </div>
       </Container>
    );
--- app/projects/styled.tsx
+++ app/projects/styled.tsx
@@ -35,16 +35,26 @@ export const ProjectContainer = styled.div`
    }
 `;
 
+export const PaginationContainer = styled.div`
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   width: 300px;
+`;
+
+export const NumberContainer = styled.div`
+   display: flex;
+`;
+
 export const NumberBox = styled.div`
    background-color: #f1f1f1;
    display: flex;
    justify-content: center;
    align-items: center;
-   height: 20px;
-   width: 20px;
+   height: 40px;
+   width: 40px;
    border-radius: 5px;
-   margin: 30px;
-   padding: 10px;
+   margin: 0 10px;
    border: 1px solid rgba(0, 0, 0, 0.1);
    cursor: pointer;
 `;
