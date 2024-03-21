d4670c3b023c30738d192949dfbd9aa80f58ef61
Pedro-dev-083 <pedroazevedo83@hotmail.com>
2024-03-20 21:48:38 -0300

    Adding Image to the ProjectModal

--- app/projects/ProjectModal/ProjectModal.tsx
+++ app/projects/ProjectModal/ProjectModal.tsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import { ProjectContainer } from "./styled";
 import Project from "../../components/Project/Project";
+import Image from "next/image";
 
 export default function ProjectModal() {
    const [visible, setVisible] = useState(false);
    return (
       <>
          <ProjectContainer onClick={() => setVisible(true)}>
+            <Image
+               src={
+                  "https://portifolio-pedro-dev-083-bucket.s3.sa-east-1.amazonaws.com/projects-images/4862756037.png"
+               }
+               alt={"An barcode"}
+               width={150}
+               height={150}
+            />
             <p>Project</p>
          </ProjectContainer>
          <Project visible={visible} setVisible={setVisible} />
